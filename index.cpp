b2c8f1e0d5e222438a6db94ccb9c3f9e415bdc0d
vaishnavi yelne <v16938372@gmail.com>
2026-04-02 10:28:43 +0530

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+// Function to evaluate postfix expression
+int evaluatePostfix(string exp) {
+ stack<int> s;
+ for (int i = 0; i < exp.length(); i++) {
+ // If operand, push into stack
+ if (isdigit(exp[i])) {
+ s.push(exp[i] - '0');
+ }
+ else {
+ int val1 = s.top();
+ s.pop();
+ int val2 = s.top();
+ s.pop();
+ switch (exp[i]) {
+ case '+': s.push(val2 + val1); break;
+ case '-': s.push(val2 - val1); break;
+ case '*': s.push(val2 * val1); break;
+ case '/': s.push(val2 / val1); break;
+ }
+ }
+ }
+ return s.top();
+}
+int main() {
+ string exp = "23*54*+9-";
+ cout << "Postfix Expression: " << exp << endl;
+ cout << "Result: " << evaluatePostfix(exp);
+ return 0;
+}
